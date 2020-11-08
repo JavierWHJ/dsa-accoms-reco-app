3b8e1a02f3339539b63996a4834dfcb8c2718ebb
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-11-08 18:48:34 +0800

    Add dependencies and boilerplate for services

---
+++ services/accomsService.js
@@ -0,0 +1,30 @@
+const getAccoms = (input) => {
+    const userInput = {
+        location: input.location,
+        ratings: input.ratings,
+        ammenities: input.ammenities,
+    }
+    return fetch("http://localhost:3030/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userInput)
+    }).then(res => {
+        return res
+    });
+}
+
+const getAmmenitiesList = () => {
+    return fetch("http://localhost:5000/ammenities")
+    .then(res => res.json())
+    .then(result => {
+        return result
+    })
+    .catch(console.log);
+}
+
+export default {
+    getAccoms,
+    getAmmenitiesList,
+}
