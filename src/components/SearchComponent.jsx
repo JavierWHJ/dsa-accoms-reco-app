935e5567b3fbbf04c5b26e18c4b18c5a155b5850
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-11-08 21:19:05 +0800

    Remove value from input

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -8,10 +8,10 @@ const SearchComponent = () => {
                 <h2 color="#2C2C2C" style={{fontSize: "30px", fontWeight: "600", margin: "0px"}}>Find your ideal accomodation!</h2>
                 <p color="#2C2C2C" style={{fontSize: "15px"}}>compares details from 200+ booking sites to help you find the right accomodation for you.</p>
                 <div className={styles.inputGroup}>
-                    <input type="text" placeholder="Location “Thailand, Asia”" class="ant-input ant-input-lg pac-target-input" value="" />
-                    <input type="date" placeholder="Start Date" class="ant-input ant-input-lg pac-target-input" value="" />
-                    <input type="date" placeholder="End Date" class="ant-input ant-input-lg pac-target-input" value="" />
-                    <input type="number" placeholder="Number of People" class="ant-input ant-input-lg pac-target-input" value="" />
+                    <input type="text" placeholder="Location “Thailand, Asia”" class="ant-input ant-input-lg pac-target-input" />
+                    <input type="date" placeholder="Start Date" class="ant-input ant-input-lg pac-target-input" />
+                    <input type="date" placeholder="End Date" class="ant-input ant-input-lg pac-target-input" />
+                    <input type="number" placeholder="Number of People" class="ant-input ant-input-lg pac-target-input" />
                     <button type="submit" class="ant-btn ant-btn-primary ant-btn-lg"><span>Find Accomodations</span></button>
                 </div>
             </div>
