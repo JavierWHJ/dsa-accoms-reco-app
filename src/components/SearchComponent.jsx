a91ad5f96b511d6c4ff0216b4cbf64936dc63c3d
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-11-08 21:41:25 +0800

    Change placeholder for input

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -10,10 +10,11 @@ const SearchComponent = () => {
                 <h2 color="#2C2C2C" style={{fontSize: "30px", fontWeight: "600", margin: "0px"}}>Find your ideal accomodation!</h2>
                 <p color="#2C2C2C" style={{fontSize: "15px"}}>compares details from 200+ booking sites to help you find the right accomodation for you.</p>
                 <div className={styles.inputGroup}>
-                    <input type="text" placeholder="Location “Thailand, Asia”" class="ant-input ant-input-lg pac-target-input" />
+                    <input type="text" placeholder="Location “Singapore, Malaysia”" class="ant-input ant-input-lg pac-target-input" />
                     <input type="date" placeholder="Start Date" class="ant-input ant-input-lg pac-target-input" />
                     <input type="date" placeholder="End Date" class="ant-input ant-input-lg pac-target-input" />
                     <input type="number" placeholder="Number of People" class="ant-input ant-input-lg pac-target-input" />
+                    
                     <Link to="/accomodations"><button type="submit" class="ant-btn ant-btn-primary ant-btn-lg"><span>Find Accomodations</span></button></Link>
                 </div>
             </div>
